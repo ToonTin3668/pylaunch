5cae31eece0c708bb7270a196301229134174628
Vincent <vinm3668+github@gmail.com>
2022-10-20 06:53:06 -0700

    started changes to switching to javascript

--- app/views.py
+++ app/views.py
@@ -7,10 +7,13 @@ views = Blueprint('views', __name__)
 @views.route('/intro', methods=["GET", "POST"])
 def intro():
     if request.method == "POST":
-        if request.form['iconButton'] == 'iconBtn':
+        if request.form['iconButton'] == 'iconbtn':
             return redirect(url_for("/"))
     return render_template("intro.html")
 
 @views.route('/', methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        if request.form['docsButton'] == 'Docs':
+            return redirect(url_for("/intro"))
     return render_template("home.html")
